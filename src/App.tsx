7652cdcb0dbb43d5a3f10fb6d9fccbb4b1a1057a
Ömer Sakmar <omersakmar@gmail.com>
2023-02-28 12:14:07 +0300

    overall rating & carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import {
+  Card,
+  Carousel,
+  CarouselItem,
+  Col,
+  Container,
+  Row,
+} from "react-bootstrap";
 import StarRatings from "react-star-ratings";
 import { images } from "./api/imageKeys";
 import "./App.scss";
@@ -9,6 +16,7 @@ import {
   resultedArr,
 } from "./lib/Helper";
 import listData from "./api/list-data.json";
+import iconFlag from "./images/icon_flag.png";
 function App() {
   const [averagePraiseRating, setAveragePraiseRating] = useState<string>("0");
   const list: any = listData;
@@ -18,38 +26,66 @@ function App() {
       0
     );
     const avgPraiseRating = totalPraiseRating / list.Row.length;
-    setAveragePraiseRating(avgPraiseRating.toFixed(2));
+    setAveragePraiseRating(avgPraiseRating.toFixed(1));
   }, []);
 
   return (
-    <div className="main">
-      <div className="container">
-        {resultedArr.map((result: object[]) =>
-          result.map((item: any) => (
-            <div className="card">
-              <div className="card-img">
-                <img
-                  style={{ width: "100px", height: "auto", objectFit: "cover" }}
-                  src={images[item.lookupId]}
-                  alt={item.lookupValue}
-                />
-              </div>
-              <div className="card-content">
-                <h3>{item.lookupValue}</h3>
+    <>
+      <Container>
+        <div className="d-flex align-items-center mb-4">
+          <div className="top-container">
+            <img src={iconFlag} alt="icon-flag" />
+            <div className="span-container">
+              <span>{averagePraiseRating}</span>
+            </div>
+          </div>
+          <div className="d-flex flex-column">
+            <p>Tüm Rozetlerde</p>
+            <div className="d-flex gap-2 align-items-center">
+              <span>
                 <StarRatings
-                  rating={3}
+                  rating={Number(averagePraiseRating)}
                   starRatedColor="orange"
                   starDimension="20px"
-                  numberOfStars={5}
-                  name="rating"
                 />
-                <p>{item.count} Adet</p>
-              </div>
+              </span>
+              <span>{badgesTotalAmount} Adet</span>
             </div>
-          ))
-        )}
-      </div>
-    </div>
+          </div>
+        </div>
+        <Carousel>
+          {resultedArr.map((result: object[], index: number) => (
+            <CarouselItem key={index}>
+              <Row>
+                {result.map((item: any) => (
+                  <Col xs={6}>
+                    <Card className="d-flex justify-content-around">
+                      <Card.Img
+                        src={images[item.lookupId]}
+                        alt={item.lookupValue}
+                        style={{ width: "52px", height: "auto" }}
+                      />
+
+                      <Card.Title>{item.lookupValue}</Card.Title>
+                      <span className="">
+                        <StarRatings
+                          rating={item.averagePraiseRating}
+                          starRatedColor="orange"
+                          starDimension="10px"
+                          numberOfStars={5}
+                          name="rating"
+                        />
+                      </span>
+                      <Card.Text>{item.count} Adet</Card.Text>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </CarouselItem>
+          ))}
+        </Carousel>
+      </Container>
+    </>
   );
 }
 
